4150c7cf54fd9ff27ae50bbfee4a0dabdda974d4
Vadim <vadim.prog.2000@gmail.com>
2020-10-05 22:53:48 +0300

    create code that adds data from object to the site and remove extra html code

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict"
+
+const model = [
+	{type: 'title', value: 'Hello World from JS'},
+	{type: 'text', value: 'Here we go with some text'},
+	{type: 'columns', value: [
+		'Hello',
+		'World',
+		'!'
+	]}
+]
+
+const $site = document.querySelector('#site');
+
+model.forEach(block => {
+	let html = ''
+
+	if (block.type === 'title') {
+		html = title(block)
+	} else if (block.type === 'text') {
+		html = text(block)
+	} else if (block.type === 'columns') {
+		html = columns(block)
+	}
+
+	$site.insertAdjacentHTML('beforeend', html)
+})
+
+function title(block) {
+	return `
+	<div class="row">
+		<div class="col-sm">
+			<h1>${block.value}</h1>
+		</div>
+	</div>
+	`
+}
+
+function text(block) {
+	return `
+	<div class="row">
+		<div class="col-sm">
+			<p>${block.value}</p>
+		</div>
+	</div>
+	`
+}
+
+function columns(block) {
+	const html = block.value.map(e=> `<div class="col-sm"><p>${e}</p></div>`).join("")
+	return `
+	<div class="row">
+		${html}
+	</div>
+	`
+}
