14eddb943879e47d0c20ab50b402c81ee47be8d0
Vadim <vadim.prog.2000@gmail.com>
2020-10-07 19:55:41 +0300

    del imports & add excep in  gitignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import {model} from './model'
-import {Site} from './classes/site.js'
 import './styles/main'
-import { Sidebar } from './classes/sidebar';
 import { App } from './classes/app';
 
 new App(model).init()
